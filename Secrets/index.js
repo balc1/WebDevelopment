0a184c49759e7fc7596f063bcac3a285d0c79281
E.Balci <111460297+balc1@users.noreply.github.com>
2024-01-31 20:58:20 +0300

    Add files via upload

---
+++ Secrets/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get("https://secrets-api.appbrewery.com/random");
+        const result = response.data;
+        console.log(result);
+        res.render("index.ejs", { secret: result.secret, user: result.username });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+        error: error.message,
+    });
+    }
+})
+
+app.listen(port, () => {
+    console.log("Server running port : " + port);
+});
